d9c97e325f4862fb361e454fca82a9333ac879f1
Karan Dodeja <41475214+Karan-Dodeja@users.noreply.github.com>
2024-12-27 23:09:55 +0530

    Created Employee DTO and Mapper

---
+++ ems-backend/src/main/java/com/karan/ems/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.karan.ems.repository;
+
+import com.karan.ems.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
