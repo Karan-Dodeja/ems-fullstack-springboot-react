bd1ca1c0f81f0be757fa21e85160b94ecb80014c
Karan Dodeja <41475214+Karan-Dodeja@users.noreply.github.com>
2024-12-30 23:31:01 +0530

    Add Footer and Header in FrontEnd

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
+import { HeaderComponent } from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
     <>
+      <HeaderComponent />
       <ListEmployeeComponent />
+      <FooterComponent />
     </>
   );
 }
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const HeaderComponent = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          Employee Management System
+        </a>
+      </nav>
+    </div>
+  );
+};
