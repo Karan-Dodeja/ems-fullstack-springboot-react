eef12171572d84da89491624f54dc8cb7ed61740
Karan Dodeja <41475214+Karan-Dodeja@users.noreply.github.com>
2025-01-02 01:35:34 +0530

    Setup Routing in frontend

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,13 +2,19 @@ import { useState } from "react";
 import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import { HeaderComponent } from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeComponent />
-      <FooterComponent />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          <Route path="/" element={<ListEmployeeComponent />} />
+          <Route path="/employees" element={<ListEmployeeComponent />} />
+        </Routes>
+        <FooterComponent />
+      </BrowserRouter>
     </>
   );
 }
