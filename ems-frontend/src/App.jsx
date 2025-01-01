118eaccc17ed749864edbdc5ab30b8502542f877
Karan Dodeja <41475214+Karan-Dodeja@users.noreply.github.com>
2025-01-02 01:49:22 +0530

    Created Employee Component and Steup routing

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import ListEmployeeComponent from "./components/ListEmployeeComponent";
 import { HeaderComponent } from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmployeeComponent from "./components/EmployeeComponent";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<ListEmployeeComponent />} />
           <Route path="/employees" element={<ListEmployeeComponent />} />
+          <Route path="/add-employee" element={<EmployeeComponent />} />
         </Routes>
         <FooterComponent />
       </BrowserRouter>
--- ems-frontend/src/components/ListEmployeeComponent.jsx
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { listEmployees } from "../services/EmployeeService";
+import { useNavigate } from "react-router-dom";
 
 function ListEmployeeComponent() {
   const [employees, setEmployees] = useState([]);
+  const navigator = useNavigate();
   useEffect(() => {
     listEmployees()
       .then((res) => {
@@ -12,9 +14,15 @@ function ListEmployeeComponent() {
         console.log(e);
       });
   }, []);
+  const addNewEmployee = () => {
+    navigator("/add-employee")
+  };
   return (
     <div className="container">
       <h2 className="text-center">ListEmployeeComponent</h2>
+      <button className="btn btn-primary mb-2" onClick={addNewEmployee}>
+        Add Employee
+      </button>
       <table className="table table-striped table-bordered">
         <thead>
           <tr>
